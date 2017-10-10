079713489c65de2d1d193118031e16b9e4fcffae
pedrohcavalcante <pedrohcavalcante@ufrn.edu.br>
2017-10-10 10:59:17 -0300

    ajuste para mostrar os links

--- deputado/views.py
+++ deputado/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post, Link
 
-def getLink(request):
+''' def getLink(request):
     links = Link.objects.all()
-    return render(request, 'deputado/post_list.html', {'link': links})
+    return render(request, 'deputado/post_list.html', {'links': links}) '''
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('nome_deputado')
-    return render(request, 'deputado/post_list.html', {'posts': posts})
+    posts = Post.objects.all()
+    links = Link.objects.all()
+    return render(request, 'deputado/post_list.html', {'posts': posts, 'links': links})
 
